b85c8ec005e79f70e1ffc8483cb4e4edc1260cf1
Nedim Muratović <nmuratovic1@etf.unsa.ba>
2017-05-24 11:52:35 +0200

    Dodao implementaciju privatnog dijela klase objava

--- Projekat/Share/Classes/Objava.cs
+++ Projekat/Share/Classes/Objava.cs
@@ -8,5 +8,63 @@ namespace Share
 {
     class Objava
     {
+        private int id;
+        public string Naziv { get; set; }
+        // Tip objave može biti: slika, zvuk ili video.
+        public string Tip { get; set; }
+        private int korisnikId;
+        public int BrojSvidjanja { get; set; }
+        // Predstavlja datum i vrijeme kada je objava kreirana
+        private DateTime datumVrijeme; 
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                /** 
+                 * @field-id se postavlja na vrijednost za 1 veću od vrijednosti id-a posljednje
+                 * kreirane objave.
+                 * Vrijednost @field-id će se automatski dodijeliti objavi pri kreiranju.
+                 */
+                id = value;
+            }
+        }
+
+        public int KorisnikId
+        {
+            get
+            {
+                return korisnikId;
+            }
+            set
+            {
+                /**
+                 * Objavu kreiraju registrovani korisnici tako da, kada se objava kreira,
+                 * znamo id korisnika koji je tu objavu kreirao (objavio).
+                 */
+                korisnikId = value;
+            }
+        }
+
+        public DateTime DatumVrijeme
+        {
+            get
+            {
+                return datumVrijeme;
+            }
+            set
+            {
+                /**
+                 * @field-datumVrijeme mora biti validna vrijednost, a bit će validna
+                 * jer će se ova vrijednost automatski kreirati kada korisnik želi
+                 * kreirati neku objavu.
+                 */
+                datumVrijeme = value;
+            }
+        }
     }
 }
