a4e291d5c27dea106f3df2333b6c187f69ae02ae
Nedim Muratović <nmuratovic1@etf.unsa.ba>
2017-05-24 11:23:43 +0200

    Dodao nekoliko klasa
    U nekoliko klasa sam dodao i atribute uz komentar o čemu treba voditi
    računa pri radu sa tim atributima.

---
+++ Projekat/Share/Classes/RegistrovaniKorisnik.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Share
+{
+    public class RegistrovaniKorisnik
+    {
+        private int id;
+        private string username;
+        private string password;
+        private string email;
+        public string Image { get; set; }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                /** 
+                 * @field-id se postavlja na vrijednost za 1 veću od vrijednosti posljednjeg 
+                 * registrovanog korisnika.
+                 * Vrijednost @field-id će se automatski dodijeliti korisniku pri registraciji.
+                 */
+                id = value;
+            }
+        }
+
+        public string Username
+        {
+            get
+            {
+                return username;
+            }
+            set
+            {
+                /**
+                 * Potrebno je provjeriti da li je @param-username kojeg korisnik unosi validan.
+                 * Ukoliko @param-username nije validan, korisnik se obavještava o neispravnom unosu.
+                 */
+
+                // Napisati provjeru
+
+                username = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                /**
+                 * Iz sigurnosnih razloga, @field-password mora sadržavati barem jedno veliko slovo,
+                 * barem jedno malo slovo i barem jedan broj pri čemu dužina @field-password mora
+                 * biti minimalno 5 znakova.
+                 */
+
+                // Napisati provjeru
+
+                password = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                /**
+                 * @field-email mora završavati sa "@hotmail.com" ili "@gmail.com".
+                 * Također, @field-email ne može biti isti kao email nekog već registrovanog
+                 * korisnika.
+                 */
+                
+                // Napisati provjeru
+
+                email = value;
+            }
+        }
+    }
+}
